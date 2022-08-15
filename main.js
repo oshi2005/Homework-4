71243f2c0226837167f624f646b6c49fd6359daa
oshi2005 <110298645+oshi2005@users.noreply.github.com>
2022-08-15 16:19:11 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+//  Let charAt
+let greeting = 'Hello. My name is John' ;
+let letter = greeting.charAt(8);
+
+
+// Let Slice
+
+// let str1 ='The morning is upon us.';
+// let result = str1.slice(12);
+// document.getElementById("result").innerHTML = result;
+
+
+
+
+//Let str2 str3 
+
+
+let str1 = "Hello";
+let str2 = "world!";
+let result = str1.concat(" ", str2,);
+
+document.getElementById("demo").innerHTML = result;
